d27fbeafc7745c66881b61c22d781fb09750d9a8
P-large <lanof01@gmail.com>
2019-07-13 20:01:42 +0000

    add Factorial function

--- app.js
+++ app.js
@@ -4,4 +4,12 @@ let sum = 0;
 for (let i = 0; i <= number; i++){
 	sum = sum + i;
 }
-console.log(sum);
+console.log(Factorial(5));
+
+function Factorial(n){
+	if(n <= 1){
+		return n;
+	}else{
+		return n * Factorial(n - 1);
+	}
+}
